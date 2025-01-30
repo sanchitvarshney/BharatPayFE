ecf9dcf9c52c7729d06086dd5431a884a38d5394
sanchitvarshney <sanchit.varshney@mscorpres.in>
2025-01-30 16:09:23 +0530

    Make R9 report issue columns dynamic and add validation on Q6 statement

--- src/pages/queries/Q6Statement.tsx
+++ src/pages/queries/Q6Statement.tsx
@@ -34,6 +34,7 @@ const Q6Statement: React.FC = () => {
               }
             }
           }}
+          inputProps={{ maxLength: 15 }}
         />
         <LoadingButton
           loadingPosition="start"
