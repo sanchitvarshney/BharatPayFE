48ba1081f6f5230a3be6f00e294e04865b9c46f3
sachinmscorpres <sachin.maurya@mscorpres.in>
2025-01-30 17:22:05 +0530

    update q6 report

--- src/pages/queries/Q6Statement.tsx
+++ src/pages/queries/Q6Statement.tsx
@@ -1,6 +1,6 @@
 import { Icons } from "@/components/icons";
 import { LoadingButton } from "@mui/lab";
-import { TextField } from "@mui/material";
+import { ListItemText, TextField } from "@mui/material";
 import Timeline from "@mui/lab/Timeline";
 import TimelineItem from "@mui/lab/TimelineItem";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
@@ -51,43 +51,55 @@ const Q6Statement: React.FC = () => {
       </div>
       <div className="h-[calc(100vh-190px)] p-[20px] overflow-y-auto">
         {q6Statement ? (
-          <Timeline
-            position="right"
-            sx={{
-              [`& .${timelineOppositeContentClasses.root}`]: {
-                flex: 0.2,
-              },
-            }}
-          >
-            {q6Statement.map((item, index) => (
-              <TimelineItem key={index}>
-                <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2" color="text.secondary">
-                  {item.time}
-                </TimelineOppositeContent>
-                <TimelineSeparator>
-                  {index !== 0 && <TimelineConnector />}
-                  <TimelineDot color="primary">
-                    <ReceiptIcon fontSize="small" />
-                  </TimelineDot>
-                  {index !== q6Statement.length - 1 && <TimelineConnector />}
-                </TimelineSeparator>
-                <TimelineContent sx={{ py: "12px", px: 2 }}>
-                  <Typography variant="h6" component="span">
-                    {item.transactionType}
-                  </Typography>
-                  <Typography>
-                  TXN ID:   {item.minNo} 
-                  </Typography>
-                  <Typography>
-                    User:  {item.user || ""} 
-                  </Typography>
-                  <Typography>
-                    Location:  {item.location || ""} 
-                  </Typography>
-                </TimelineContent>
-              </TimelineItem>
-            ))}
-          </Timeline>
+          <div className="flex ">
+            <div className="w-full">
+              <Timeline
+                position="right"
+                sx={{
+                  [`& .${timelineOppositeContentClasses.root}`]: {
+                    flex: 0.2,
+                  },
+                }}
+              >
+                {q6Statement.map((item, index) => (
+                  <TimelineItem key={index}>
+                    <TimelineOppositeContent sx={{ m: "auto 0" }} align="right" variant="body2" color="text.secondary">
+                      {item.time}
+                    </TimelineOppositeContent>
+                    <TimelineSeparator>
+                      {index !== 0 && <TimelineConnector />}
+                      <TimelineDot color="primary">
+                        <ReceiptIcon fontSize="small" />
+                      </TimelineDot>
+                      {index !== q6Statement.length - 1 && <TimelineConnector />}
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ py: "12px", px: 2 }}>
+                      <Typography variant="h6" component="span">
+                        {item.transactionType}
+                      </Typography>
+                      <Typography>TXN ID: {item.minNo}</Typography>
+                      <Typography>User: {item.user || ""}</Typography>
+                      <Typography>Location: {item.location || ""}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </Timeline>
+            </div>
+            <div className="max-w-max">
+              <ListItemText
+                primary={"IMEI"}
+                secondary={q6Statement[0]?.imei}
+                primaryTypographyProps={{ fontSize: "18px", fontWeight: "bold" }} // Increase primary text size
+                secondaryTypographyProps={{ fontSize: "1rem", color: "gray" }} // Increase secondary text size
+              />
+              <ListItemText
+                primary={"Seral No."}
+                secondary={q6Statement[0]?.serial}
+                primaryTypographyProps={{ fontSize: "1.5rem", fontWeight: "bold" }} // Increase primary text size
+                secondaryTypographyProps={{ fontSize: "1rem", color: "gray" }} // Increase secondary text size
+              />
+            </div>
+          </div>
         ) : (
           <div className="flex items-center justify-center h-full">
             <img src="/search.svg" className="w-[300px] opacity-60 " alt="" />
