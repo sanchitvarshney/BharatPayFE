0331ca9421bb44a5898b138a9124aff752c7b6ca
sanchitvarshney <sanchit.varshney@mscorpres.in>
2025-02-05 13:03:25 +0530

    Add Indate in R9 report

--- src/table/report/R9ReportTable.tsx
+++ src/table/report/R9ReportTable.tsx
@@ -18,6 +18,7 @@ const R9ReportTable: React.FC<Props> = ({ gridRef }) => {
     { headerName: "#", field: "id", valueGetter: "node.rowIndex+1", maxWidth: 100 },
     { headerName: "Vendor Code", field: "vendorCode" },
     { headerName: "Vendor Name", field: "vendorName", minWidth: 300 },
+    { headerName: "In Date", field: "inDate", minWidth: 200 },
     { headerName: "Vendor Address", field: "vendorAddress", minWidth: 400 },
     { headerName: "AWB No", field: "awbNo" },
     { headerName: "Serial", field: "serial" },
