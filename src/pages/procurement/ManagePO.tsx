369e0fd5681b6d1ad8d9bf21454fda30448a4328
sanchitvarshney <sanchit.varshney@mscorpres.in>
2025-05-21 11:08:08 +0530

    Add Manage PO UI and add ellipese icon and add things

--- src/pages/procurement/ManagePO.tsx
+++ src/pages/procurement/ManagePO.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useState } from "react";
-
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import dayjs, { Dayjs } from "dayjs";
 import { ColDef } from "@ag-grid-community/core";
 import { OverlayNoRowsTemplate } from "@/components/reusable/OverlayNoRowsTemplate";
@@ -10,25 +10,20 @@ import { AgGridReact } from "@ag-grid-community/react";
 
 import LoadingButton from "@mui/lab/LoadingButton";
 import SearchIcon from "@mui/icons-material/Search";
-import { FormControl, Menu, MenuItem, Select, TextField } from "@mui/material";
-
+import { FormControl, IconButton, Menu, MenuItem, Select, TextField } from "@mui/material";
 import { showToast } from "@/utils/toasterContext";
-
 import { Icons } from "@/components/icons";
 import MuiTooltip from "@/components/reusable/MuiTooltip";
 import { rangePresets } from "@/utils/rangePresets";
 import { Button } from "@/components/ui/button";
-
 import CustomLoadingOverlay from "@/components/reusable/CustomLoadingOverlay";
 import RangeSelect from "@/components/reusable/antSelecters/RangeSelect";
-import { setDateRange } from "@/features/procurement/poSlices";
+import {  setDateRange } from "@/features/procurement/poSlices";
 const ManagePO: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const dispatch = useAppDispatch();
-  const { managePoData, loading, dateRange } = useAppSelector(
-    (state) => state.po
-  );
+  const { managePoData, loading,dateRange } = useAppSelector((state) => state.po);
   const [colapse, setcolapse] = useState<boolean>(false);
   const [type, setType] = useState<string>("datewise");
   //   const [detail, setDetail] = useState<boolean>(false);
@@ -37,16 +32,35 @@ const ManagePO: React.FC = () => {
     from: null,
     to: null,
   });
-  const [selectedRow, setSelectedRow] = useState<any>(null);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [open, setOpen] = useState<boolean>(false);
+  const [selectedRow, setSelectedRow] = useState<any>(null);
+console.log(selectedRow,'selectedRow');
 
-  // console.log("managePoData", managePoData);
-  //   const dispatch = useAppDispatch();
-
-  //   const gridRef = useRef<AgGridReact<any>>(null);
+  const handleMenuClick = (
+    event: React.MouseEvent<HTMLElement>,
+    rowData: any
+  ) => {
+    setAnchorEl(event.currentTarget);
+    setSelectedRow(rowData);
+  };
 
   const columnDefs: ColDef[] = [
+    {
+      headerName: "",
+      // pinned: "right",
+      sortable: false,
+      filter: false,
+      cellRenderer: (params: { data: any }) => (
+        <IconButton
+          size="small"
+          onClick={(e) => handleMenuClick(e, params.data)}
+          className="hover:bg-gray-100"
+        >
+          <MoreVertIcon className="h-4 w-4" />
+        </IconButton>
+      ),
+      width: 50,
+    },
     {
       headerName: "#",
       field: "id",
@@ -102,17 +116,13 @@ const ManagePO: React.FC = () => {
 
   const handlePageChange = useCallback(
     (page: number) => {
-      console.log("handlePageChange", dateRange);
-      // if (!dateRange) {
-      //   console.log("no date range");
-      //   return;
-      // }
-
+     
       setCurrentPage(page);
       dispatch(
         getListofPo({
           wise: type,
           data: dateRange,
+
           page: page,
           limit: pageSize,
         })
@@ -146,187 +156,185 @@ const ManagePO: React.FC = () => {
   };
 
   const handleExport = () => {
-    console.log("DownloadReport");
-    //   if (!dateRange || !date) {
-    //     return showToast("Please select location and date range", "error");
-    //   }
-    //   const reportPayload = {
-    //     partner: partner,
-    //     fromDate: dateRange.from?.format("DD-MM-YYYY"),
-    //     toDate: dateRange.to?.format("DD-MM-YYYY"),
-    //   };
-    //   swipeMachineInward(reportPayload);
-    //   showToast("Start downloading ", "success");
+    console.log("DownloadReport")
   };
-  console.log("date range", dateRange);
+  console.log("date range", dateRange)
 
   const handlePrintChallan = () => {
     dispatch(poPrint({ id: selectedRow.po_transaction }));
   };
 
   return (
-    <div className="flex bg-white h-[calc(100vh-100px)] relative">
-      <div
-        className={`transition-all flex flex-col gap-[10px] h-[calc(100vh-100px)]  border-r border-neutral-300   ${
-          colapse ? "min-w-0 max-w-0" : "min-w-[400px] max-w-[400px] "
-        }`}
-      >
+  
+      <div className="flex bg-white h-[calc(100vh-100px)] relative">
         <div
-          className={`transition-all ${
-            colapse ? "left-0" : "left-[400px]"
-          } w-[16px] p-0  h-full top-0 bottom-0 absolute rounded-none  text-slate-600 z-[10] flex items-center justify-center`}
+          className={`transition-all flex flex-col gap-[10px] h-[calc(100vh-100px)]  border-r border-neutral-300   ${
+            colapse ? "min-w-0 max-w-0" : "min-w-[400px] max-w-[400px] "
+          }`}
         >
-          <Button
-            onClick={() => setcolapse(!colapse)}
-            className={`transition-all w-[16px] p-0 py-[35px] bg-neutral-200  rounded-none hover:bg-neutral-300/50 text-slate-600 hover:h-full shadow-sm shadow-neutral-400 duration-300   `}
+          <div
+            className={`transition-all ${
+              colapse ? "left-0" : "left-[400px]"
+            } w-[16px] p-0  h-full top-0 bottom-0 absolute rounded-none  text-slate-600 z-[10] flex items-center justify-center`}
           >
-            {colapse ? (
-              <Icons.right fontSize="small" />
-            ) : (
-              <Icons.left fontSize="small" />
-            )}
-          </Button>
-        </div>
-        <div className="overflow-x-hidden overflow-y-auto ">
-          <div className="flex items-center gap-[10px] p-[10px]  mt-[20px]">
-            <FormControl fullWidth>
-              <Select
-                value={type}
-                defaultValue="min"
-                onChange={(e) => setType(e.target.value)}
-              >
-                {[
-                  { value: "powise", label: "PO", isDisabled: false },
-                  { value: "datewise", label: "Date", isDisabled: false },
-                  { value: "vendorwise", label: "Vender", isDisabled: false },
-                ].map((item) => (
-                  <MenuItem
-                    disabled={item.isDisabled}
-                    value={item.value}
-                    key={item.value}
-                  >
-                    {item.label}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+            <Button
+              onClick={() => setcolapse(!colapse)}
+              className={`transition-all w-[16px] p-0 py-[35px] bg-neutral-200  rounded-none hover:bg-neutral-300/50 text-slate-600 hover:h-full shadow-sm shadow-neutral-400 duration-300   `}
+            >
+              {colapse ? (
+                <Icons.right fontSize="small" />
+              ) : (
+                <Icons.left fontSize="small" />
+              )}
+            </Button>
           </div>
-          <div className=" p-[10px]">
-            {type === "powise" ? (
-              <div className="flex flex-col gap-[20px] ">
-                <TextField
-                  label="PO"
-                  value={po}
-                  onChange={(e) => setPo(e.target.value)}
-                />
-
-                <div className="flex items-center justify-between">
-                  <LoadingButton
-                    className="max-w-max"
-                    variant="contained"
-                    loading={loading}
-                    onClick={() => {
-                      if (po) {
-                        dispatch(
-                          getListofPo({
-                            wise: "powise",
-                            data: po,
-                            limit: pageSize,
-                            page: 1,
-                          })
-                        );
-                      } else {
-                        showToast("Please enter PO", "error");
-                      }
-                    }}
-                    startIcon={<SearchIcon fontSize="small" />}
-                  >
-                    Search
-                  </LoadingButton>
-                  <MuiTooltip title="Download" placement="right">
-                    <LoadingButton
-                      // disabled={!mainR1Report}
-                      variant="contained"
-                      color="primary"
-                      style={{
-                        borderRadius: "50%",
-                        width: 40,
-                        height: 40,
-                        minWidth: 0,
-                        padding: 0,
-                      }}
-                      onClick={handleExport}
-                      size="small"
-                      sx={{ zIndex: 1 }}
+          <div className="overflow-x-hidden overflow-y-auto ">
+            <div className="flex items-center gap-[10px] p-[10px]  mt-[20px]">
+              <FormControl fullWidth>
+                <Select
+                  value={type}
+                  defaultValue="min"
+                  onChange={(e) => setType(e.target.value)}
+                >
+                  {[
+                    { value: "powise", label: "PO", isDisabled: false },
+                    { value: "datewise", label: "Date", isDisabled: false },
+                    { value: "vendorwise", label: "Vender", isDisabled: false },
+                  ].map((item) => (
+                    <MenuItem
+                      disabled={item.isDisabled}
+                      value={item.value}
+                      key={item.value}
                     >
-                      <Icons.download />
-                    </LoadingButton>
-                  </MuiTooltip>
-                </div>
-              </div>
-            ) : type === "datewise" ? (
-              <div className="flex flex-col gap-[20px] ">
-                <RangeSelect
-                  value={date}
-                  onChange={handleDateChange}
-                  disabledDate={(current) => {
-                    return current ? current > dayjs() : false;
-                  }}
-                  format="DD/MM/YYYY"
-                  presets={rangePresets}
-                  placeholder={["Start Date", "End Date"]}
-                />
-                <div className="flex justify-between">
-                  <LoadingButton
-                    loadingPosition="start"
-                    onClick={() => {
-                      if (!date.from || !date.to) {
-                        showToast("Please select date range", "error");
-                      } else {
-                        let dataString = "";
+                      {item.label}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
+            <div className=" p-[10px]">
+              {type === "powise" ? (
+                <div className="flex flex-col gap-[20px] ">
+                  <TextField
+                    label="PO"
+                    value={po}
+                    onChange={(e) => setPo(e.target.value)}
+                  />
 
-                        const startDate = dayjs(date.from).format("DD-MM-YYYY");
-                        const endDate = dayjs(date.to).format("DD-MM-YYYY");
-                        dataString = `${startDate}-${endDate}`;
-                        dispatch(setDateRange(dataString as any));
-
-                        dispatch(
-                          getListofPo({
-                            wise: "datewise",
-                            data: dataString,
-                            limit: pageSize,
-                            page: 1,
-                          })
-                        );
-                      }
-                    }}
-                    variant="contained"
-                    loading={loading}
-                    startIcon={<SearchIcon fontSize="small" />}
-                  >
-                    Search
-                  </LoadingButton>
-                  <MuiTooltip title="Download" placement="right">
+                  <div className="flex items-center justify-between">
                     <LoadingButton
+                      className="max-w-max"
                       variant="contained"
-                      color="primary"
-                      style={{
-                        borderRadius: "50%",
-                        width: 40,
-                        height: 40,
-                        minWidth: 0,
-                        padding: 0,
+                      loading={loading}
+                      onClick={() => {
+                        if (po) {
+                          dispatch(
+                            getListofPo({
+                              wise: "powise",
+                              data: po,
+                              limit: pageSize,
+                              page: 1,
+                            })
+                          );
+                        } else {
+                          showToast("Please enter PO", "error");
+                        }
                       }}
-                      onClick={() => {}}
-                      size="small"
-                      sx={{ zIndex: 1 }}
+                      startIcon={<SearchIcon fontSize="small" />}
                     >
-                      <Icons.download />
+                      Search
                     </LoadingButton>
-                  </MuiTooltip>
+                    <MuiTooltip title="Download" placement="right">
+                      <LoadingButton
+                        // disabled={!mainR1Report}
+                        variant="contained"
+                        color="primary"
+                        style={{
+                          borderRadius: "50%",
+                          width: 40,
+                          height: 40,
+                          minWidth: 0,
+                          padding: 0,
+                        }}
+                        onClick={handleExport}
+                        size="small"
+                        sx={{ zIndex: 1 }}
+                      >
+                        <Icons.download />
+                      </LoadingButton>
+                    </MuiTooltip>
+                  </div>
                 </div>
-              </div>
-            ) : null}
+              ) : type === "datewise" ? (
+                <div className="flex flex-col gap-[20px] ">
+                  <RangeSelect
+                    value={date}
+                    onChange={handleDateChange}
+                    disabledDate={(current) => {
+                      return current ? current > dayjs() : false;
+                    }}
+                    format="DD/MM/YYYY"
+                    presets={rangePresets}
+                    placeholder={["Start Date", "End Date"]}
+                  />
+                  <div className="flex justify-between">
+                    <LoadingButton
+                      loadingPosition="start"
+                      onClick={() => {
+                        if (!date.from || !date.to) {
+                          showToast("Please select date range", "error");
+                        } else {
+                          let dataString = "";
+
+                          const startDate = dayjs(date.from).format(
+                            "DD-MM-YYYY"
+                          );
+                          const endDate = dayjs(date.to).format("DD-MM-YYYY");
+                          dataString = `${startDate}-${endDate}`;
+                          dispatch(setDateRange(dataString as any) );
+
+                          dispatch(
+                            getListofPo({
+                              wise: "datewise",
+                              //   from: dayjs(date.from).format("DD-MM-YYYY"),
+                              //   to: dayjs(date.to).format("DD-MM-YYYY"),
+                              data: dataString,
+                              limit: 2,
+                              page: 1,
+                            })
+                          );
+                        }
+                      }}
+                      variant="contained"
+                      loading={loading}
+                      //   disabled={!date || mainR1ReportLoading}
+                      startIcon={<SearchIcon fontSize="small" />}
+                    >
+                      Search
+                    </LoadingButton>
+                    <MuiTooltip title="Download" placement="right">
+                      <LoadingButton
+                        // disabled={!mainR1Report}
+                        variant="contained"
+                        color="primary"
+                        style={{
+                          borderRadius: "50%",
+                          width: 40,
+                          height: 40,
+                          minWidth: 0,
+                          padding: 0,
+                        }}
+                        onClick={() => {}}
+                        size="small"
+                        sx={{ zIndex: 1 }}
+                      >
+                        <Icons.download />
+                      </LoadingButton>
+                    </MuiTooltip>
+                  </div>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
         <div className="w-full">
@@ -353,37 +361,36 @@ const ManagePO: React.FC = () => {
               />
             </div>
           </div>
-          <Menu
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={() => setAnchorEl(null)}
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "right",
-            }}
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-          >
-            <MenuItem
+        </div>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={() => setAnchorEl(null)}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem
             // onClick={handleEditChallan}
             // disabled={selectedRow ? isDispatchCreated(selectedRow) : false}
-            >
-              Update PO
-            </MenuItem>
-            <MenuItem
+          >
+            Update PO
+          </MenuItem>
+          <MenuItem
             // onClick={handleCreateDispatch}
             // disabled={selectedRow ? isDispatchCreated(selectedRow) : false}
-            >
-              View
-            </MenuItem>
-            <MenuItem onClick={handlePrintChallan}>Download</MenuItem>
-            <MenuItem onClick={handlePrintChallan}>Cancel</MenuItem>
-          </Menu>
-        </div>
+          >
+            View 
+          </MenuItem>
+          <MenuItem onClick={handlePrintChallan}>Download</MenuItem>
+          <MenuItem onClick={handlePrintChallan}>Cancel</MenuItem>
+        </Menu>
       </div>
-    </div>
   );
 };
 
