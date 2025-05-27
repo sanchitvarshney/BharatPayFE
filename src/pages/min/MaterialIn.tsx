ae6c6492add73408843927d7921c9550f70b5f01
sanchitvarshney <sanchit.varshney@mscorpres.in>
2025-05-27 17:58:37 +0530

    Modify code

--- src/pages/min/MaterialIn.tsx
+++ src/pages/min/MaterialIn.tsx
@@ -125,6 +125,7 @@ const MaterialIn: React.FC = () => {
   };
 
   const onSubmit: SubmitHandler<any> = (data) => {
+    console.log(data);
     if (!storeInvoiceFiles)
       return showToast("Please Upload Invoice File", "error");
     // dispatch(storeFormdata(data));
