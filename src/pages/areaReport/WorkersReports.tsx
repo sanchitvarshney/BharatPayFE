411fd0907756135ff7734a481cc12e45408accc1
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-02-05 14:24:41 +0530

    Add filters in workers report

--- src/pages/areaReport/WorkersReports.tsx
+++ src/pages/areaReport/WorkersReports.tsx
@@ -1,16 +1,16 @@
-import CustomLoadingOverlay from '@/components/reusable/CustomLoadingOverlay';
-import { OverlayNoRowsTemplate } from '@/components/reusable/OverlayNoRowsTemplate';
-import { toast } from '@/components/ui/use-toast';
-import { getWorkerReport } from '@/features/areaSlice/areaSlice';
-import { rangePresets } from '@/utils/rangePresets';
-import { LoadingButton, Skeleton } from '@mui/lab';
-import { ColDef } from 'ag-grid-community';
-import { AgGridReact } from 'ag-grid-react';
-import { DatePicker } from 'antd';
-import dayjs from 'dayjs';
-import React, {  useMemo, useRef } from 'react'
-import { Controller, useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+import CustomLoadingOverlay from "@/components/reusable/CustomLoadingOverlay";
+import { OverlayNoRowsTemplate } from "@/components/reusable/OverlayNoRowsTemplate";
+import { toast } from "@/components/ui/use-toast";
+import { getWorkerReport } from "@/features/areaSlice/areaSlice";
+import { rangePresets } from "@/utils/rangePresets";
+import { LoadingButton, Skeleton } from "@mui/lab";
+import { ColDef } from "@ag-grid-community/core";
+import { AgGridReact } from "@ag-grid-community/react";
+import { DatePicker } from "antd";
+import dayjs from "dayjs";
+import React, { useMemo, useRef } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
 
 const { RangePicker } = DatePicker;
 
@@ -27,89 +27,98 @@ const columnDefs: any[] = [
     headerName: "Date",
     field: "date",
     minWidth: 200,
-  
+
     flex: 1,
   },
   {
     headerName: "Swipe (Production / Sales) Cost",
-     children: [
+    children: [
       {
         headerName: "P12",
         field: "swipe_p12",
-      
-      }, {
+      },
+      {
         headerName: "P10",
-        field: "swipe_p10"
-      }, {
+        field: "swipe_p10",
+      },
+      {
         headerName: "G2 4G",
-        field: "swipe_g24g"
-      }, {
+        field: "swipe_g24g",
+      },
+      {
         headerName: "Ingenico",
-        field: "swipe_ingenico"
-      }, {
+        field: "swipe_ingenico",
+      },
+      {
         headerName: "Assembly",
-        field: "swipe_assembly"
-      }, {
+        field: "swipe_assembly",
+      },
+      {
         headerName: "Store + Admin",
-        field: "swipe_storeAdmin"
+        field: "swipe_storeAdmin",
       },
       {
         headerName: "Cost",
-        field: "swipe_cost"
-      }, {
+        field: "swipe_cost",
+      },
+      {
         headerName: "Profit / Loss",
-        field: "swipe_profitLoss"
-      }
+        field: "swipe_profitLoss",
+      },
     ],
-   
- 
+
     minWidth: 200,
   },
   {
     headerName: "Sound Box (Production / Sales) Cost",
-      children: [
+    children: [
       {
         headerName: "Production",
-        field: "soundbox_production"
-      },  {
+        field: "soundbox_production",
+      },
+      {
         headerName: "Assembly / Cost",
-        field: "soundbox_assemblycost"
-      },{
+        field: "soundbox_assemblycost",
+      },
+      {
         headerName: "TRC",
-        field: "soundbox_trc"
-      }, {
+        field: "soundbox_trc",
+      },
+      {
         headerName: "Store + Admin",
-        field: "soundbox_storeAdmin"
+        field: "soundbox_storeAdmin",
       },
       {
         headerName: "Total Cost",
-        field: "soundbox_totalcost"
-      }, {
+        field: "soundbox_totalcost",
+      },
+      {
         headerName: "Profit / Loss",
-        field: "soundbox_profitLoss"
-      }
+        field: "soundbox_profitLoss",
+      },
     ],
     field: "",
- 
+
     minWidth: 200,
   },
   {
     headerName: "Cleaning Production",
-     children: [
+    children: [
       {
         headerName: "Production",
-        field: "cleaning_production"
-      }, 
+        field: "cleaning_production",
+      },
       {
         headerName: "Cost",
-        field: "cleaning_cost"
-      }, {
+        field: "cleaning_cost",
+      },
+      {
         headerName: "Profit / Loss",
-        field: "cleaning_profitLoss"
-      }
+        field: "cleaning_profitLoss",
+      },
     ],
     field: "",
- 
+
     minWidth: 200,
   },
   {
@@ -117,18 +126,19 @@ const columnDefs: any[] = [
     children: [
       {
         headerName: "Production",
-        field: "preqc_production"
-      }, 
+        field: "preqc_production",
+      },
       {
         headerName: "Cost",
-        field: "preqc_cost"
-      }, {
+        field: "preqc_cost",
+      },
+      {
         headerName: "Profit / Loss",
-        field: "preqc_production"
-      }
+        field: "preqc_production",
+      },
     ],
     field: "",
- 
+
     minWidth: 200,
   },
   {
@@ -136,21 +146,23 @@ const columnDefs: any[] = [
     children: [
       {
         headerName: "Production",
-        field: "gtotal_production"
-      }, 
+        field: "gtotal_production",
+      },
       {
         headerName: "Cost",
-        field: "gtotal_cost"
-      },   {
+        field: "gtotal_cost",
+      },
+      {
         headerName: "Contribution Cost",
-        field: "gtotal_contribution"
-      },{
+        field: "gtotal_contribution",
+      },
+      {
         headerName: "Difference",
-        field: "gtotal_difference"
-      }
+        field: "gtotal_difference",
+      },
     ],
     field: "",
- 
+
     minWidth: 200,
   },
   {
@@ -158,58 +170,74 @@ const columnDefs: any[] = [
     children: [
       {
         headerName: "Staff Cost",
-        field: "contribution_staff"
-      }, 
+        field: "contribution_staff",
+      },
       {
         headerName: "Depreciation",
-        field: "contribution_depreciation"
-      }, {
+        field: "contribution_depreciation",
+      },
+      {
         headerName: "Consumable",
-        field: "contribution_consumable"
-      }, {
+        field: "contribution_consumable",
+      },
+      {
         headerName: "Other",
-        field: "contribution_other"
-      }, {
+        field: "contribution_other",
+      },
+      {
         headerName: "Total",
-        field: "contribution_total"
-      }
+        field: "contribution_total",
+      },
     ],
     field: "",
- 
+
     minWidth: 200,
   },
-
 ];
 
 const WorkersReports = () => {
   const gridRef = useRef(null);
-      const dispatch: any = useDispatch();
+  const dispatch: any = useDispatch();
   const {
     workerReports,
- 
+
     isReportLoading,
   } = useSelector((state: any) => state.placeMaster);
- 
 
   const defaultColDef = useMemo<ColDef>(() => {
     return {
       filter: "agTextColumnFilter",
       floatingFilter: true,
       sortable: true,
-      
+
       resizable: true,
-            cellRenderer: (params: any) => {
-        return params.value === null || params.value === undefined ? 0 : params.value;
+      cellRenderer: (params: any) => {
+        return params.value === null || params.value === undefined
+          ? 0
+          : params.value;
       },
-      cellStyle: { textAlign: 'center' },
+      cellStyle: { textAlign: "center" },
     };
   }, []);
-  //   const sideBar = useMemo(() => {
-  //   return {
-  //     toolPanels: ["columns", "filters"],
-  //     defaultToolPanel: "columns",
-  //   };
-  // }, []);
+  const sideBar = useMemo(
+    () => ({
+      toolPanels: [
+        {
+          id: "columns",
+          labelDefault: "Columns",
+          labelKey: "columns",
+          iconKey: "columns",
+          toolPanel: "agColumnsToolPanel",
+          toolPanelParams: {
+            suppressPivotMode: true,
+            suppressPivots: true,
+          },
+        },
+      ],
+      defaultToolPanel: "",
+    }),
+    []
+  );
 
   const {
     control,
@@ -217,7 +245,6 @@ const WorkersReports = () => {
     formState: { errors },
   } = useForm<any>({
     defaultValues: {
-  
       date: null,
     },
   });
@@ -226,7 +253,6 @@ const WorkersReports = () => {
     const payload: any = {
       from: dayjs(data?.date[0]).format("DD-MM-YYYY"),
       to: dayjs(data?.date[1]).format("DD-MM-YYYY"),
-    
     };
 
     //@ts-ignore
@@ -248,21 +274,19 @@ const WorkersReports = () => {
       }
     });
   };
- 
-
- 
 
   return (
-      <div className="grid  w-full grid-cols-[1fr_3fr]  bg-white">
+    <div className="grid  w-full grid-cols-[1fr_3fr]  bg-white">
       <div className="w-full border-r border-neutral-300">
         <form onSubmit={handleSubmit(onSubmit)} className="p-[10px]">
           <div className="py-[0px] flex flex-col gap-[0px]">
             <div>
-              <label className="text-[14px] font-[500] text-slate-600 ">Select Date Range</label>
+              <label className="text-[14px] font-[500] text-slate-600 ">
+                Select Date Range
+              </label>
               <Controller
                 name="date"
                 control={control}
-
                 rules={{
                   required: "Date range is required",
                 }}
@@ -286,17 +310,13 @@ const WorkersReports = () => {
                 </span>
               )}
             </div>
-          
           </div>
           <div className="h-[50px] p-0 flex items-center px-[20px] gap-[10px] justify-end">
-         
             <LoadingButton
               loadingPosition="start"
               type="submit"
               variant="contained"
-             
-              disabled={isReportLoading }
-
+              disabled={isReportLoading}
             >
               Search
             </LoadingButton>
@@ -308,12 +328,12 @@ const WorkersReports = () => {
           ref={gridRef}
           loadingOverlayComponent={CustomLoadingOverlay}
           suppressCellFocus={true}
+          suppressMenuHide={true}
           overlayNoRowsTemplate={OverlayNoRowsTemplate}
           rowData={workerReports ?? []}
           loading={isReportLoading}
-           sideBar={"columns"}
+          sideBar={sideBar}
           columnDefs={columnDefs}
-
           defaultColDef={defaultColDef}
           pagination={true}
           paginationPageSize={50}
@@ -324,7 +344,7 @@ const WorkersReports = () => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WorkersReports
+export default WorkersReports;
