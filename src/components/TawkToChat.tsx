8c0dc4e840cca50845a7181dc0c01da021547ad6
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-03-20 17:40:25 +0530

    refactorices code

--- src/components/TawkToChat.tsx
+++ src/components/TawkToChat.tsx
@@ -167,6 +167,7 @@ export default function TawkToChatOakter() {
       "a[title*='Add Chat to your website'] { display: none !important; }",
       ".tawk-branding { display: none !important; }",
       "[class*='tawk-branding'] { display: none !important; }",
+        "[class*='tawk-bottom-navbar'] {  bottom: 0px !important; }",
          "[class*='tawk-toolbar-menu'] { display: none !important; }",
       ".tawk-padding-small { display: none !important; }",
     ].join("\n");
