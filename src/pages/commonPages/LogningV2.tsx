9108f8be0f8d0d03529b18d3ed557d4826ee3367
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-05-09 13:28:31 +0530

    resolve issue

--- src/pages/commonPages/LogningV2.tsx
+++ src/pages/commonPages/LogningV2.tsx
@@ -38,6 +38,7 @@ import { useNavigate } from "react-router-dom";
 import { consumeReturnTo } from "@/utils/returnTo";
 import ReCAPTCHA from "react-google-recaptcha";
 import { GoogleLogin } from "@react-oauth/google";
+import SelectEndPoint from "@/components/shared/SelectEndPoint";
 const LogningV2: React.FC = () => {
   const [showPassword, setShowPassword] = React.useState<boolean>(false);
   const [recaptchaValue, setRecaptchaValue] = React.useState<string | null>(
@@ -220,8 +221,8 @@ const LogningV2: React.FC = () => {
           </SwiperSlide>
         </Swiper>
       </div>
-      <div className="relative flex items-center justify-center w-full h-full">
-    
+      <div className="relative flex items-center justify-center w-full h-full flex-col">
+        <SelectEndPoint/>
         <Card elevation={4} sx={{ width: "500px", padding: "20px" }}>
           <Typography
             color="primary"
