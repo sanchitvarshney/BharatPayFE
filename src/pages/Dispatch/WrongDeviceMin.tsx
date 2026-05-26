b5ed49670b3626a5f9ed6bafdeb3e022b808b6a7
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-05-26 12:40:36 +0530

    Add trackon in delhivery partner

--- src/pages/Dispatch/WrongDeviceMin.tsx
+++ src/pages/Dispatch/WrongDeviceMin.tsx
@@ -49,6 +49,7 @@ const PartnerData = [
   { value: "ShadowFox", text: "ShadowFox" },
   { value: "GMS", text: "GMS" },
   { value: "BLUEDART", text: "BLUEDART" },
+  { value: "Trackon", text: "Track On" },
 ];
 
 // AWB Length validation mapping based on partner
