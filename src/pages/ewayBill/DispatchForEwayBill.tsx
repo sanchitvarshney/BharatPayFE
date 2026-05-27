6f8a89a5cf06f14d8c75fc25c54ad4b488795edd
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-05-27 16:10:51 +0530

    disable Eway bill  types also add type during create bill

--- src/pages/ewayBill/DispatchForEwayBill.tsx
+++ src/pages/ewayBill/DispatchForEwayBill.tsx
@@ -140,9 +140,9 @@ const DispatchForEwayBill: React.FC = () => {
                 id="demo-simple-select"
                 label="Device Type"
               >
-                <MenuItem value={"soundBox"}>Sound Box</MenuItem>
+                {/* <MenuItem value={"soundBox"}>Sound Box</MenuItem>
                 <MenuItem value={"swipeMachine"}>Swipe Machine</MenuItem>
-                <MenuItem value={"wrongDevices"}>Wrong Device</MenuItem>
+                <MenuItem value={"wrongDevices"}>Wrong Device</MenuItem> */}
                  <MenuItem value={"PART"}>Part Code</MenuItem>
               </Select>
             </FormControl>
