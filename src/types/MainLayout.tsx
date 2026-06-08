ce33646691da19ddd575396ad53043471a2122f8
sanchitvarshney <sanchit.varshney@mscorpres.in>
2026-06-08 14:25:57 +0530

    Revert "Merge pull request #129 from sanchitvarshney/sidebar/new-sidebar"
    This reverts commit 74a171773f3a4fbb579a96fc0d68b44a91bb1fe0, reversing
    changes made to 29d4c70e4664ecbaca9cbfab2b55cfa897309b66.

--- src/types/MainLayout.tsx
+++ src/types/MainLayout.tsx
@@ -3,18 +3,29 @@ import { Dispatch, SetStateAction, RefObject } from "react";
 export interface MainUIStateType {
   sheetOpen: boolean;
   setSheetOpen: Dispatch<SetStateAction<boolean>>;
+  sheet2Open: boolean;
+  setSheet2Open: Dispatch<SetStateAction<boolean>>;
+  favoriteSheet: boolean;
+  setFavoriteSheet: Dispatch<SetStateAction<boolean>>;
   notificationSheet: boolean;
   setNotificationSheet: Dispatch<SetStateAction<boolean>>;
   logotAlert: boolean;
   setLogotAlert: Dispatch<SetStateAction<boolean>>;
+  favoriteLinkList:FavoriteMenuLinkListType[];
+  setFavoriteLinkList: Dispatch<SetStateAction<FavoriteMenuLinkListType[]>>;
   modalRef: RefObject<HTMLDivElement>;
-  sidebarWidth: number;
+  sidebaref: RefObject<HTMLDivElement>;
+  favoriteref: RefObject<HTMLDivElement>;
 }
 
 export interface Props {
   uiState: MainUIStateType;
-  menu?:SidebarMenuLinkType;
-  headerHeight?: number;
+  menu?:SidebarMenuLinkType
+}
+
+export interface  FavoriteMenuLinkListType {
+  name:string;
+  path:string
 }
 
 export  interface SidebarMenuLinkType {
