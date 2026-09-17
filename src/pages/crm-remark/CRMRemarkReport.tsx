b34cf986c4b5e1c3a6ebdeaf31e92d52c84b9d79
deepakMS12 <deepak.dhiman@mscorpres.in>
2026-09-17 16:06:19 +0530

    added awb col in CRM Report

--- src/pages/crm-remark/CRMRemarkReport.tsx
+++ src/pages/crm-remark/CRMRemarkReport.tsx
@@ -73,6 +73,12 @@ const CRMRemarkReport: React.FC = () => {
       minWidth: 200,
       headerClass: "font-semibold",
     },
+        {
+      headerName: "AWB No.",
+      field: "awb_no",
+      minWidth: 200,
+      headerClass: "font-semibold",
+    },
          {
       headerName: "Remark Updated Date",
       field: "date",
